c24f56bba5d53100b250f5834a9543a06b6234dd
Llamero <benjamin.smith@maine.edu>
2020-01-23 23:39:38 -0800

    Sync to computer time on upload, add sensor system variables

--- Datalogger_Driver/Datalogger_Driver.ino
+++ Datalogger_Driver/Datalogger_Driver.ino
@@ -4,8 +4,7 @@
 #include "Adafruit_TSL2591.h" //Light Sensor
 #include "LCD.h" //Display
 #include <Wire.h> //I2C
-
-Adafruit_TSL2591 tsl = Adafruit_TSL2591(2591); // pass in a number for the sensor identifier (for your use later)
+#include <TimeLib.h> //Set RTC time and get time strings
 
 //Setup LCD pin numbers
 const int DB_pin_array[] = {32, 31, 8, 6, 5, 4, 3, 1}; //List of 4 (DB4-DB7) or 8 (DB0-DB7) pins to send data to LCD
@@ -18,18 +17,66 @@ const int LED_PWM_pin = 29; //Drive LED backlight intensity
 const int contrast_pin = A21; //DAC pin for addjusting diplay contrast
 const int n_DB_pin = sizeof(DB_pin_array)/sizeof(DB_pin_array[0]);
 
-LCD lcd(DB_pin_array, n_DB_pin, RS_pin, RW_pin, E_pin, LCD_toggle_pin, LED_PWM_pin, contrast_pin);
+//Setup sensor pin numbers
+const int temp_power_pin = 20; //Set Vcc pins
+const int color_power_pin = 23;
+const int light_power_pin = 17;
+const int color_interrupt_pin = 16; //Set interrupt pins
+const int light_interrupt_pin = 33;
+const TwoWire* temp_port = &Wire; //Set I2C (wire) ports
+const TwoWire* color_port = &Wire;
+const TwoWire* light_port = &Wire1;
+
+//Arrays for storing display Strings
+String boot_array[5] = {"Boot status:", "", "", "", ""}; //Array for boot display
+
+//Initialize libraries
+Adafruit_BME280 temp_sensor; //Create instance of temp sensor
+Adafruit_TCS34725 color_sensor = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_60X); //Create instance of color sensor initialize with peak sensitivity
+Adafruit_TSL2591 light_sensor = Adafruit_TSL2591(2591); //Create instance of light sensor - number is sensor ID
+LCD lcd(DB_pin_array, n_DB_pin, RS_pin, RW_pin, E_pin, LCD_toggle_pin, LED_PWM_pin, contrast_pin); //Create instance of LCD display
+
+time_t t = 0;
 float inc = 0;
 float contrast = 0;
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  analogWriteResolution(12);
-  Wire1.begin();
-  //Wire1.setClock(400000);
+  //Power on the sensors
+  pinMode(temp_power_pin, OUTPUT);
+  pinMode(color_power_pin, OUTPUT);
+  pinMode(light_power_pin, OUTPUT);
+  digitalWriteFast(temp_power_pin, HIGH);
+  digitalWriteFast(color_power_pin, HIGH);
+  digitalWriteFast(light_power_pin, HIGH);
+    
+  //Setup USB serial communication
+  Serial.begin(9600); //Baud rate is ignored and negotiated with computer for max speed
   wakeUSB();
-  lcd.initializeLCD();
-  pinMode(LED_BUILTIN, OUTPUT);
+  
+  //Setup I2C communication - sensor libraries will initialize I2C communications
+  Wire.setClock(400000); //Set I2C clock speeds to max that chips support
+  Wire1.setClock(400000);
+
+  //Start LCD display
+  analogWriteResolution(16); //Set DAC and PWM resolution
+  lcd.initializeLCD(); //Start LCD display
+
+  //Synchronize Teensy clock to computer clock
+  setSyncProvider(getTeensy3Time); //Sync to computer clock - do not use () in function as argument
+  if (timeStatus()!= timeSet) { //If sync failed
+    boot_array[1] = "RTC time sync fail:";
+  } else { // if Sync successful
+    boot_array[1] = "RTC sync successful:";
+  }
+  t = now();
+  boot_array[2] = timeString(t); //Write current time to boot screen
+  
+  
+  
+  while(!Serial);
+  Serial.println(boot_array[0]);
+  Serial.println(boot_array[1]);
+  Serial.println(boot_array[2]);
+  pinMode(LED_BUILTIN, OUTPUT); //Turn off LED
   digitalWriteFast(LED_BUILTIN, LOW);
 }
 
@@ -38,11 +85,37 @@ void loop() {
   lcd.setLCDcontrast(contrast);
   inc = inc + 0.02;
   if(inc>2*PI) inc = 0;
-  tsl.begin(&Wire1);
+  light_sensor.begin(light_port); //Specify I2C port when initializaing library
   digitalWriteFast(LED_BUILTIN, HIGH);
   delayMicroseconds(100);
   digitalWriteFast(LED_BUILTIN, LOW);
   delayMicroseconds(100);
+}
+
+time_t getTeensy3Time(){
+  return Teensy3Clock.get();
+}
+
+//Create a time string formated as "year month day hour:minute:second"
+String timeString(time_t unix_t){
+  String t = "";
+  t += year(unix_t);
+  t += " ";
+  if(month(unix_t) < 10) t += "0";
+  t += month(unix_t);
+  t += " ";
+  if(day(unix_t) < 10) t+= "0";
+  t += day(unix_t); 
+  t += " "; 
+  if(hour(unix_t) < 10) t+= "0";
+  t += hour(unix_t);
+  t += ":";
+  if(minute(unix_t) < 10) t+= "0";
+  t += minute(unix_t);
+  t += ":";  
+  if(second(unix_t) < 10) t+= "0";
+  t += second(unix_t);
+  return t; 
 }
 
 void wakeUSB(){
